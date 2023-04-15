927f33abd6925903aaf98dc2b845bea4e00a597d
CarolinaJCJ <111032921+CarolinaJCJ@users.noreply.github.com>
2023-04-15 13:53:39 -0400

    Update product.router.js

--- src/Routes/product.router.js
+++ src/Routes/product.router.js
@@ -8,35 +8,52 @@ const producto = new ProductManager()
 
 //GET
 router.get('/', async(req, res) => {
+
     const prod =  await producto.getProducts()
     const limit = req.query.limit
     if(!limit) return res.send(prod)
     res.send(prod.slice(0,limit))
 })
 
+// GET /api/products
 router.get('/:pid', async(req, res) => {
     const id = parseInt(req.params.pid)
     const prod =  await producto.getProductById(id)
-    if(!prod) return res.send({error: 'No se encuentra el producto'})
+
+    if(!prod) return res.status(404).send({error: 'No se encuentra el producto'})
     res.send(prod)
 })
 
 //POST
 router.post('/', async(req, res) => {
-    let prod = req.body
-    if(!prod.title || !prod.description) {
-        return res.status(400).send({status: 'error', messaje: 'Todos los campos son necesarios'})
+    let payload = req.body
+    if(!payload.code) {
+        return res.status(400).send({status: 'error', message: 'El campo \'code\' es requerido'})
     }
-    res.send({status: "Sucess", messaje: await producto.addProduct(prod)})
+
+    try {
+        const newProduct = await producto.addProducts(payload)
+        res.send(newProduct)
+    } catch (error) {
+        if (error.message && error.code) {
+            res.status(400)
+                .send({
+                    status: 'error',
+                    message: error.message,
+                });
+        } else {
+            throw error;
+        }
+    }
     //res.status(200).send({prod})
 })
 
 //PUT
 router.put('/:pid', async(req, res) =>{
     const pid = parseInt(req.params.pid)
-    const prod = req.body
-    await producto.updateProduct(pid, prod)
-    res.send(producto)
+    const payload = req.body
+    const product = await producto.updateProduct(pid, payload)
+    res.send(product)
 })
 
 //DELETE
@@ -45,4 +62,4 @@ router.delete('/:pid', async(req, res) =>{
     res.send({status: "Success", message: await producto.deleteProduct(pid)})
 })
 
-export default {router}
+export default router
